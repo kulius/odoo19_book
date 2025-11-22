4de6dec07382b57ae71eafba4eb6b1f18aa5120e
kulius <kullius@msn.com>
2025-11-22 22:14:36 +0800

    刪除 custom_test2026 和 odoo_test2025 模組

--- custom_test2026/models/sale_order.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    custom_123 = fields.Char(string='客戶備註')
--- odoo_test2025/models/sale_order.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    custom_ref = fields.Char(string='Customer Note')
