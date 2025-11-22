0599866e500ac4dfba209077b59637b85e992405
kulius <kulius@users.noreply.github.com>
2025-11-22 15:47:02 +0800

    Add custom reference field to sale orders

---
+++ odoo_test2025/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Odoo Test 2025',
+    'version': '1.0.0',
+    'summary': 'Add custom reference field on sale orders.',
+    'depends': ['sale'],
+    'data': [
+        'views/sale_order_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
