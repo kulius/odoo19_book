820e5405ab3cfb869eeb5b9f7162c29a9e5635d1
kulius <kulius@users.noreply.github.com>
2025-11-22 15:54:46 +0800

    Add custom_test2026 module for sale order note

---
+++ custom_test2026/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Custom Test 2026',
+    'version': '1.0.0',
+    'summary': 'Adds customer note field on sale orders.',
+    'depends': ['sale'],
+    'data': [
+        'views/sale_order_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
